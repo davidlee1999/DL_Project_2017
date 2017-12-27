85e394605d1eec6307022b62ba94a53761b7627c
David Lee <1360230697@qq.com>
2017-12-27 12:39:20 +0800

    Just to test my pycharm

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-a = 3
-b = 5
-print(a+b)
+import tensorflow as tf
+a = tf.Variable(3, dtype = tf.int32)
+b = tf.Variable(5, dtype = tf.int32)
+c = tf.Variable(0, dtype = tf.int32)
+c = tf.add(a,3)
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(c))
